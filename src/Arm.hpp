49b5976128c2f8da62711a2986c32379acd06bf6
mattolse592 <mattolse592.ed.ambsb.ca>
2024-10-29 08:59:44 -0400

    adds pid controller for arm movement generated with chat gpt. Don't knwo if it work.

--- src/Arm.hpp
+++ src/Arm.hpp
@@ -3,6 +3,7 @@
 
 #include "Motor.hpp"
 #include "RotationSensor.hpp"
+#include "PIDGPT.hpp"
 
 class Arm
 {
@@ -10,13 +11,15 @@ class Arm
 private:
     Motor Motor_;
     RotationSensor RotationSensor_;
+    PIDController pid_;
+
 public:
     enum State
     {
-        DOCK=0,
-        LOAD=1,
-        REACH=2,
-        SCORE=3
+        DOCK = 0,
+        LOAD = 1,
+        REACH = 2,
+        SCORE = 3
     };
 
 private:
@@ -24,13 +27,19 @@ private:
 
 public:
     Arm(Motor Motor, RotationSensor RotationSensor)
-        : Motor_(Motor), RotationSensor_(RotationSensor)
+        : Motor_(Motor), RotationSensor_(RotationSensor), pid_(1.0, 0.1, 0.05, 0.0)
     {
         Motor_.SetBrakeMode(MOTOR_BRAKE_BRAKE);
     }
 
     void Tick()
     {
+        double current_position = RotationSensor_.GetPosition();
+        double pid_output = pid_.Calculate(current_position);
+
+        // Use the output from PID to set motor speed
+        Motor_.SetSpeed(static_cast<int>(pid_output));
+
         switch (target)
         {
         case DOCK:
@@ -49,11 +58,12 @@ public:
         default:
             break;
         }
+        //manual move probably doesn't work
     }
 
     void ManualMove(int stickInput)
     {
-        Motor_.SetSpeed(stickInput);        
+        Motor_.SetSpeed(stickInput / 4);
     }
 
     void SetTarget(State state)
@@ -64,18 +74,22 @@ public:
 private:
     void Dock()
     {
+        pid_.setTarget(0.0);
     }
 
     void Load()
     {
+        pid_.setTarget(15.0);
     }
 
     void Reach()
     {
+        pid_.setTarget(115.0);
     }
 
     void Score()
     {
+        pid_.setTarget(165.0);
     }
 };
-#endif  // ARM_HPP
+#endif // ARM_HPP
